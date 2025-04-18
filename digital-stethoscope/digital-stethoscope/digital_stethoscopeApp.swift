8609fc4786aab16fbbeca361f2a8415eec544dae
sirajpal <sirajpal@ucsd.edu>
2025-04-18 15:21:27 -0700

    re-create xcode skeleton - downgraded

---
+++ digital-stethoscope/digital-stethoscope/digital_stethoscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  digital_stethoscopeApp.swift
+//  digital-stethoscope
+//
+//  Created by Siya Rajpal on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct digital_stethoscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ digital-stethoscope/digital-stethoscopeTests/digital_stethoscopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  digital_stethoscopeTests.swift
+//  digital-stethoscopeTests
+//
+//  Created by Siya Rajpal on 4/18/25.
+//
+
+import Testing
+@testable import digital_stethoscope
+
+struct digital_stethoscopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

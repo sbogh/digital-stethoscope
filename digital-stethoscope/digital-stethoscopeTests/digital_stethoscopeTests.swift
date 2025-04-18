2ab5d8c5b352639d14585a4f714ed90d648d2846
sirajpal <sirajpal@ucsd.edu>
2025-04-18 15:25:32 -0700

    fixed formatting issues

--- digital-stethoscope/digital-stethoscopeTests/digital_stethoscopeTests.swift
+++ digital-stethoscope/digital-stethoscopeTests/digital_stethoscopeTests.swift
@@ -5,13 +5,11 @@
 //  Created by Siya Rajpal on 4/18/25.
 //
 
-import Testing
 @testable import digital_stethoscope
-
+import Testing
 struct digital_stethoscopeTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }

66943e97a3063c67aee87075146b408700c54b6b
sirajpal <sirajpal@ucsd.edu>
2025-04-29 19:56:33 -0700

    Minor changes to account setups, etc

---
+++ digital-stethoscope/digital-stethoscope/Views/DeviceQView.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceQView.swift
+//  digital-stethoscope
+//
+//  Created by Siya Rajpal on 4/29/25.
+//
+
+import SwiftUI
+
+struct DeviceQView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceQView()
+}

d35a99c74c616f8e496db03f4faadd49c985272a
sirajpal <sirajpal@ucsd.edu>
2025-05-01 14:25:51 -0700

    lint fixes

--- backend/firestore.py
+++ backend/firestore.py
@@ -4,11 +4,3 @@ cred = credentials.Certificate("backend/config/serviceAccountKey.json")
 initialize_app(cred)
 
 db = firestore.client()
-
-doc_ref = db.collection("users").document("V8e2dGZ6HDZuuc6MkRYK")
-
-# Update existing fields or add new ones
-doc_ref.update({
-    "name": "Updated Name",
-    "last_login": "2025-04-29"
-})

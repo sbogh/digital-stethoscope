a2119bfdfa7c1a54bcb95cdf3b52ca8c149e746a
sirajpal <sirajpal@ucsd.edu>
2025-05-01 14:31:05 -0700

    added comments, update .gitignore

--- backend/firestore.py
+++ backend/firestore.py
@@ -1,3 +1,6 @@
+"""Handles Firestore database operations for the backend."""
+
+
 from firebase_admin import credentials, initialize_app, firestore
 
 cred = credentials.Certificate("backend/config/serviceAccountKey.json")

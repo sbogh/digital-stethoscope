f6712c000e3f453a0dd103f827a103eb13aa1f0e
sirajpal <sirajpal@ucsd.edu>
2025-05-02 15:00:59 -0700

    added user object, updated create account for actual new user flow

--- digital-stethoscope/digital-stethoscope/Views/LoginFlow/CreateAccountView.swift
+++ digital-stethoscope/digital-stethoscope/Views/LoginFlow/CreateAccountView.swift
@@ -207,24 +207,25 @@ struct CreateAccountView: View {
         
         return true
     }
-    func authorizeNewUser(email: String, password: String) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                errorMessage = error.localizedDescription
-                authorized = false
-            } else {
-                errorMessage = ""
-                authorized = true
-                
-                authResult?.user.getIDToken { token, error in
-                    if let token = token {
-                        print("Got firebase token:", token)
-                        // TODO: Send token to backend
-                    }
-                }
-            }
-        }
-    }
+//    func authorizeNewUser(email: String, password: String) {
+//        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+//            if let error = error {
+//                errorMessage = error.localizedDescription
+//                authorized = false
+//            } else {
+//                errorMessage = ""
+//                authorized = true
+//                
+//                print("user authorized with email: ", userProfile.email)
+//                authResult?.user.getIDToken { token, error in
+//                    if let token = token {
+//                        print("Got firebase token:", token)
+//                        // TODO: Send token to backend
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
 
 #Preview {

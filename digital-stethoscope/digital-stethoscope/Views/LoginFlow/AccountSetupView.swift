db36491c2bab41a639e42d94d23936154ad6be20
Shelby Myrman <myrmanshelby@gmail.com>
2025-05-10 16:24:12 -0700

    changes: -fixed bulleted list under create pw -increased min font to 13 (per apple best practices)

--- digital-stethoscope/digital-stethoscope/Views/LoginFlow/AccountSetupView.swift
+++ digital-stethoscope/digital-stethoscope/Views/LoginFlow/AccountSetupView.swift
@@ -46,7 +46,7 @@ struct AccountSetupView: View {
                 if nameEmpty {
                     HStack {
                         Text("Name is required")
-                            .font(Font.custom("Roboto-Regular", size: 12))
+                            .font(Font.custom("Roboto-Regular", size: 13))
                             .foregroundColor(.red)
                             .padding(.horizontal)
                             .multilineTextAlignment(.center)
@@ -80,7 +80,7 @@ struct AccountSetupView: View {
                 if zoneEmpty {
                     HStack {
                         Text("Time Zone is required")
-                            .font(Font.custom("Roboto-Regular", size: 12))
+                            .font(Font.custom("Roboto-Regular", size: 13))
                             .foregroundColor(.red)
                             .padding(.horizontal)
                             .multilineTextAlignment(.center)

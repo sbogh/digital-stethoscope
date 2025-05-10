5501c4a6cf56d57e49ad2c134e6808dc71124cd9
Shelby Myrman <myrmanshelby@gmail.com>
2025-05-10 16:04:10 -0700

    Refactored: -turned login header into reusable component for easier editing -organized views folder

---
+++ digital-stethoscope/digital-stethoscope/Views/LearnMore.swift
@@ -0,0 +1,18 @@
+//
+//  LearnMore.swift
+//  digital-stethoscope
+//
+//  Created by Shelby Myrman on 5/10/25.
+//
+
+import SwiftUI
+
+struct LearnMore: View {
+    var body: some View {
+        Text("Learn More")
+    }
+}
+
+#Preview {
+    LearnMore()
+}

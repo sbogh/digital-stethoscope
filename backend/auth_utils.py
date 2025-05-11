d64e10129068eafef1ccfa27312f40444f0b94fe
sirajpal <sirajpal@ucsd.edu>
2025-05-11 16:21:41 -0700

    lintfix

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -4,10 +4,16 @@ from fastapi import Request, HTTPException
 from firebase_admin import auth
 
 def verify_token(request: Request) -> str:
+    """
+        Verifies token sent to backend by frontend.
+
+        Input:
+            Auth request containing current user token.
+
+    """
     auth_header = request.headers.get("Authorization")
     if not auth_header or not auth_header.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Missing or invalid token")
-    
     curr_token = auth_header.split(" ")[1]
 
     try:
--- backend/user_routes.py
+++ backend/user_routes.py
@@ -1,21 +1,52 @@
+"""
+main.py
+
+Defines all routes needed to communicate with Firestore database
+"""
+
+
 from firestore import db
 
 users = db.collection("users")
 
 def create_new_user(user_id: str, data: dict):
+    """
+        Creates a new user document in the Firestore 'users' collection.
+
+        Input:
+            user_id (str): The Firebase UID of the user.
+            data (dict): The user data to store. This dictionary is modified to include 'userID'.
+
+    """
     data["userID"] = user_id
     users.document(user_id).set(data)
 
-
 def get_user(user_id: str):
+    """
+        Retrieves a user document from the Firestore 'users' collection.
+
+        Input:
+            user_id (str): The Firebase UID of the user.
+
+        Returns:
+            dict: The user data as a dictionary if the document exists.
+            None: If the user document does not exist.
+    """
     data = users.document(user_id).get()
 
     if data.exists:
         return data.to_dict()
-    
     return None
 
 def update_current_user_device(user_id: str, device: str):
+    """
+        Updates the 'currentDeviceID' field of the specified user's document in Firestore.
+
+        Input:
+            user_id (str): The Firebase UID of the user.
+            device (str): The new device ID to set as the current device.
+
+    """
     user_reference = users.document(user_id)
     curr_data = user_reference.get()
 

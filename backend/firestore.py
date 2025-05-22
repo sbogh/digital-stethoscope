1b9a36d672fbc73a324cda7cf72e93f48ee9d4af
sirajpal <sirajpal@ucsd.edu>
2025-05-22 13:29:48 -0700

    update service account key

--- backend/firestore.py
+++ backend/firestore.py
@@ -2,6 +2,10 @@
 
 
 from firebase_admin import credentials, initialize_app, firestore
+import os
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/serviceAccountKey.json"
 
 cred = credentials.Certificate("config/serviceAccountKey.json")
 initialize_app(cred)
